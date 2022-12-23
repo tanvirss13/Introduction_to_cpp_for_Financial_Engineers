2ffc922f2e7bc735867e406b86b9b0ea76974278
tanvirss13 <79608956+tanvirss13@users.noreply.github.com>
2022-12-22 19:40:00 -0500

    Create Inequalities.hpp

---
+++ Chapter 2/Inequalities_Example/Inequalities.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#ifndef INEQUALITIES_HPP
+#define INEQUALITIES_HPP
+
+// Max and Min function 
+double Max(double x, double y);
+double Min(double x, double y);
+
+// Max and Min for 3 numbers 
+double Max(double x, double y, double z);
+double Min(double x, double y, double z);
+
+
+
+#endif //
